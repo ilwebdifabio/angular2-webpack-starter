0995246cde0059a2c167ebf39e4cb7a02a4d0fb3
Fabio Bedini <io@ilwebdifabio.it>
2016-11-12 17:48:18 +0100

    first commit

---
+++ src/vendor.browser.ts
@@ -0,0 +1,21 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+
+
+// RxJS
+import 'rxjs';
+
+if ('production' === ENV) {
+  // Production
+
+
+} else {
+  // Development
+
+}
